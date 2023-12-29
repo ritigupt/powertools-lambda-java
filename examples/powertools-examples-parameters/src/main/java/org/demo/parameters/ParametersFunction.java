bb1aa76998bfe356b3aefff481166bdd04ba9db8
Scott Gerring <gerrings@amazon.com>
2023-12-29 16:17:30 +0100

    Rejig parameters example to use the CRT client more heavily

--- examples/powertools-examples-parameters/src/main/java/org/demo/parameters/ParametersFunction.java
+++ examples/powertools-examples-parameters/src/main/java/org/demo/parameters/ParametersFunction.java
@@ -14,6 +14,7 @@
 
 package org.demo.parameters;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static software.amazon.lambda.powertools.parameters.transform.Transformer.base64;
 import static software.amazon.lambda.powertools.parameters.transform.Transformer.json;
@@ -57,31 +58,18 @@ public class ParametersFunction implements RequestHandler<APIGatewayProxyRequest
             .withTransformationManager(new TransformationManager())
             .build();
 
-    String simpleValue = ssmProvider.withMaxAge(30, SECONDS).get("/powertools-java/sample/simplekey");
-    String listValue = ssmProvider.withMaxAge(60, SECONDS).get("/powertools-java/sample/keylist");
-    MyObject jsonObj = ssmProvider.withTransformation(json).get("/powertools-java/sample/keyjson", MyObject.class);
-    Map<String, String> allValues = ssmProvider.getMultiple("/powertools-java/sample");
-    String b64value = ssmProvider.withTransformation(base64).get("/powertools-java/sample/keybase64");
-
-    Map<String, String> secretJson =
-            secretsProvider.withTransformation(json).get("/powertools-java/userpwd", Map.class);
-    MyObject secretJsonObj = secretsProvider.withMaxAge(42, SECONDS).withTransformation(json)
-            .get("/powertools-java/secretcode", MyObject.class);
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
 
-        log.info("\n=============== SSM Parameter Store ===============");
-        log.info("simplevalue={}, listvalue={}, b64value={}\n", simpleValue, listValue, b64value);
-        log.info("jsonobj={}\n", jsonObj);
+        String simpleValue = ssmProvider.withMaxAge(1, MILLIS).get("/powertools-java/sample/simplekey");
+        String listValue = ssmProvider.withMaxAge(1, MILLIS).get("/powertools-java/sample/keylist");
+        String jsonObj = ssmProvider.withMaxAge(1, MILLIS).get("/powertools-java/sample/keyjson");
+        Map<String, String> allValues = ssmProvider.withMaxAge(1, MILLIS).getMultiple("/powertools-java/sample");
+
 
         log.info("allvalues (multiple):");
         allValues.forEach((key, value) -> log.info("- {}={}\n", key, value));
-
-        log.info("\n=============== Secrets Manager ===============");
-        log.info("secretjson:");
-        secretJson.forEach((key, value) -> log.info("- {}={}\n", key, value));
-        log.info("secretjsonobj={}\n", secretJsonObj);
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
@@ -89,18 +77,14 @@ public class ParametersFunction implements RequestHandler<APIGatewayProxyRequest
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
 
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
-        }
+        //final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+        String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", "bloop");
+
+        return response
+                .withStatusCode(200)
+                .withBody(output);
+
     }
 
     private String getPageContents(String address) throws IOException {
@@ -110,3 +94,4 @@ public class ParametersFunction implements RequestHandler<APIGatewayProxyRequest
         }
     }
 }
+
